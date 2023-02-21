fb9b32c7a12a19fbdb1e573194a59546f1e5fe4b
philipquan <quan.p@northeastern.edu>
2023-02-21 11:05:30 -0800

    Refactored the script to be, hopefully, more readable

--- data-parsing/main.py
+++ data-parsing/main.py
@@ -1,18 +1,83 @@
-from typing import Dict, List, Tuple
+from typing import Callable, Any
+from datetime import datetime, timezone
 import re
 import sys
 import os
 
-REQUIRED_ARG_LENGTH = 3
+headerMap = {
+    "name": "BeerName",
+    "beerId": "BeerId",
+    "brewerId": "BrewerId",
+    "ABV": "ABV",
+    "style": "Style",
+    "appearance": "Appearance",
+    "aroma": "Aroma",
+    "palate": "Palate",
+    "taste": "Taste",
+    "overall": "Overall",
+    "time": "Created",
+    "profileName": "Username",
+    "text": "Text",
+}
 
-HEADER_PATTERN = r"[{\s]'\w+/(\w+)':.+?[,}]"
-JSON_PATTERN = r"[{\s]'\w+/(\w+)':\s['\"](.+?)['\"][,}]"
+RecordStrategy = Callable[[dict[str, Any]], str]
 
-REVIEW_START_INDEX = 5
-PROFILE_NAME_INDEX = 11
-REGEX_VALUE_INDEX = 1
+class Table:
+    def __init__(self, tableName: str, columns: list[str], parseStrategy: RecordStrategy):
+        self.tableName = tableName
+        self.columns = columns
+        self.parseStrategy = parseStrategy
+        self.data: set[str] = set()
+
+    def add(self, record: dict[str, Any]) -> None:
+        """Adds a record to the table."""
+        self.data.add(self.parseStrategy(self.columns, record))
+
+    def writeToFile(self) -> None:
+        """Writes the table to a csv file."""
+        fileName = self.tableName.lower().replace(" ", "")
+        extension = "csv"
+        print(f"Writing {fileName}.{extension}... ", end="")
+        with open(f"{fileName}.{extension}", "w") as file:
+            file.write(",".join(self.columns))
+            file.write("\n")
+            file.write("\n".join(list(self.data)))
+        print("done")
+
+_surroundWithQuotes = lambda x: f'"{x}"'
+
+def _epochToDateTime(text: str) -> str:
+    """Converts a number in string format to datetime format \"YY-MM-DD HH:MM:SS\""""
+    if not text.isnumeric():
+        raise Exception("text isn't an epoch time")
+    return datetime.fromtimestamp(int(text), timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+
+mysqlFormatters: dict[str, Callable[[str], str]] = {
+    "BeerName": _surroundWithQuotes,
+    "Style": _surroundWithQuotes,
+    "Created": _epochToDateTime,
+    "Username": _surroundWithQuotes,
+    "Text": _surroundWithQuotes,
+}
+
+def _keyStrategy(keys: list[str], record: dict[str, Any]) -> str:
+    """Extracts all key values from record into a single string formatted for CSV serialization."""
+    NULL_VALUES = ["-", ""]
+
+    wanted = []
+    for key in keys:
+        value = record[key]
+        if value in NULL_VALUES:
+            wanted.append("NULL")
+        elif key in mysqlFormatters:
+            wanted.append(mysqlFormatters[key](value))
+        else:
+            wanted.append(value)
+
+    return ",".join(wanted)
 
 def main(data_path: str, output_directory: str) -> None:
+    """Entry point."""
     if not os.path.isfile(data_path):
         print(f"{output_directory} is not a valid file.")
         return
@@ -28,65 +93,67 @@ def main(data_path: str, output_directory: str) -> None:
     print("done")
 
     print("Creating data collection entities")
-    headers: List[str] = re.findall(HEADER_PATTERN, lines[0])
-    headers = [f"{item[0].upper()}{item[1:]}" for item in headers]
 
-    beers: Dict[str, object] = {}
-    beers["header"] = ",".join(headers[:REVIEW_START_INDEX])
+    entities: list[tuple[str, list[str], RecordStrategy]] = [
+        ("Users", ["Username"], _keyStrategy),
+        ("Brewers", ["BrewerId"], _keyStrategy),
+        ("Beers", 
+            [
+                "BeerId",
+                "BeerName",
+                "ABV",
+                "BrewerId",
+                "Style"
+            ],
+        _keyStrategy),
+        ("BeerReviews",
+            [
+                # "ReviewId", we'll have MySQL auto generate ReviewId.
+                "Appearance",
+                "Aroma",
+                "Palate",
+                "Taste",
+                "Overall",
+                "Created",
+                "Text",
+                "Username",
+                "BeerId"
+            ],
+         _keyStrategy),
+        ("BeerStyles", ["Style"], _keyStrategy),
+    ]
 
-    reviews: List[str] = []
-    reviews.append(",".join(["BeerName"] + headers[REVIEW_START_INDEX:]))
+    tables: list[Table] = []
+    for tableName, columns, strategy in entities:
+        tables.append(Table(tableName, columns, strategy))
 
-    users = set()
+    record: dict[str, Any] = {}
 
-    def format_entry(items: List[str]) -> str:
-        return ",".join(f'"{item[REGEX_VALUE_INDEX]}"' for item in items)
-
+    JSON_PATTERN = r"[{\s]'\w+/(\w+)':\s['\"](.+?)['\"][,}]"
     pattern = re.compile(JSON_PATTERN)
     print("Starting parsing")
     for i, line in enumerate(lines):
         if i % 100000 == 0 and i != 0:
             print(f"Parsed {i} lines")
         try:
-            regex_result: List[Tuple[str, str]] = pattern.findall(line)
+            regex_result: list[tuple[str, str]] = pattern.findall(line)
 
-            beer_attributes = regex_result[:REVIEW_START_INDEX]
-            entry = format_entry(beer_attributes)
+            for key, value in regex_result:
+                record[headerMap[key]] = value
 
-            BEER_NAME_INDEX = 0
-            beer_name = beer_attributes[BEER_NAME_INDEX][REGEX_VALUE_INDEX]
-            if not beer_name in beers:
-                beers[beer_name] = entry
-
-            review_attributes = [beer_attributes[BEER_NAME_INDEX]] + regex_result[REVIEW_START_INDEX:]
-            entry = format_entry(review_attributes)
-            reviews.append(entry)
-
-            users.add(regex_result[PROFILE_NAME_INDEX][1])
+            for table in tables:
+                table.add(record)
         except IndexError as e:
             break
         except Exception as e:
             print(e)
             print(f"Error while parsing line: {i}\nExiting program")
             return
-    print("Done parsing")
 
-    print("Writing beers.csv... ", end="")
-    with open("beers.csv", "w") as file:
-        file.write("\n".join(beers.values()))
-    print("done")
+    for table in tables:
+        table.writeToFile()
 
-    print("Writing reviews.csv... ", end="")
-    with open("reviews.csv", "w") as file:
-        file.write("\n".join(reviews))
-    print("done")
-
-    print("Writing users.csv... ", end="")
-    with open("users.csv", "w") as file:
-        file.write("Username\n")
-        file.write("\n".join(list(users)))
-    print("done")
-
+REQUIRED_ARG_LENGTH = 3
 if len(sys.argv) != REQUIRED_ARG_LENGTH:
     print("Invalid call. Please provide the source json file and output directory as arguments.")
     exit()
