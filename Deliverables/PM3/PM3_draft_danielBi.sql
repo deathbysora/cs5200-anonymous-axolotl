81574f00a6870041be259e21d6a18939734a79a2
philipquan <quan.p@northeastern.edu>
2023-03-02 10:28:54 -0800

    Fixed comments and mispelled names

--- Deliverables/PM3/PM3_draft_danielBi.sql
+++ Deliverables/PM3/PM3_draft_danielBi.sql
@@ -1,6 +1,6 @@
-USE BeerApplication;
+USE BeerApp;
 
-# 1 What are the top 10 most reviewed beer from the reviews
+# 1 What are the top 10 most reviewed beers.
 SELECT BeerName, COUNT(*) AS Review_Counts
 FROM BeerReviews INNER JOIN Beers
   ON BeerReviews.BeerId = Beers.BeerId
@@ -8,7 +8,8 @@ GROUP BY Beers.BeerId, BeerName
 ORDER BY Review_Counts DESC
 LIMIT 10;
 
-# 2 What are the top 10 most viewed beers by users (will be populated as user using it)
+# 2 What are the top 10 most viewed beers.
+# Currently returns nothing as the table is empty.
 SELECT BeerName, COUNT(*) AS View_Counts
 # need to populate this table
 FROM ViewHistory INNER JOIN Beers
@@ -17,14 +18,15 @@ GROUP BY Beers.BeerId, BeerName
 ORDER BY View_Counts DESC
 LIMIT 10;
 
-#3 What are the top 10 users that reviewed the most amount of beers (like a leader board)
+# 3 What are the top 10 users that reviewed the most amount of beers.
+# WARNING takes long.
 SELECT Username, COUNT(*) AS UserName_Counts
 FROM BeerReviews
 GROUP BY UserName
 ORDER BY UserName_Counts DESC
 LIMIT 10;
 
-#4 What are the most 10 popular reviews that has the most comments
+# 4 What are the top 10 most popular reviews that has the most comments.
 SELECT ReviewId, COUNT(*) AS Review_Engagement
 FROM BeerComments
 GROUP BY ReviewId
@@ -32,48 +34,49 @@ ORDER BY Review_Engagement DESC
 LIMIT 10;
 
 
-#5 For a certain type of beerstyle, what are some recommended food that goes with it
-SELECT FoodName 
-from Food
-WHERE Style = 'Ale'; # we can do an enum callout?
+# 5 For a certain type of beerstyle, what are some recommended food that goes with it
+SELECT FoodName
+FROM Food
+WHERE Style REGEXP ".*ale.*"; -- we can do an enum callout?
 
 
-#6 Top 10 most active users in 2023 (cross review comments and user engagements/view history)
+# 6 Top 10 most active users in 2023 (cross review comments and user engagements/view history)
 # Elsa working on it rn
 
 
-#7 What are the top 5 most reviewed beers in the summer season (May-Sept)
+# 7 What are the top 5 most reviewed beers in the summer season (May-Sept)
+# WARNING takes long.
 SELECT BeerName, COUNT(*) AS Review_Counts
 FROM BeerReviews INNER JOIN Beers
   ON BeerReviews.BeerId = Beers.BeerId
-WHERE (MONTH(Created) <= 9 and MONTH(Created) >= 5)
+WHERE (MONTH(Created) <= 9 AND MONTH(Created) >= 5)
 GROUP BY Beers.BeerId, BeerName
 ORDER BY Review_Counts DESC
 LIMIT 5;
 
-#8 What are the top 5 most VIEWED beers in the winter season (Oct-Dec)
+# 8 What are the top 5 most VIEWED beers in the winter season (Oct-Dec)
 SELECT BeerName, COUNT(*) AS View_Counts
 # need to populate this table
 FROM ViewHistory INNER JOIN Beers
   ON ViewHistory.BeerId = Beers.BeerId
-WHERE (MONTH(Created) <= 12 and MONTH(Created) >= 10)
+WHERE (MONTH(Created) <= 12 AND MONTH(Created) >= 10)
 GROUP BY Beers.BeerId, BeerName
 ORDER BY View_Counts DESC
 LIMIT 5;
 
-#9 Which users have never created a review or viewed any beers.
+# 9 Which users have never created a review or viewed any beers.
 SELECT Users.UserName
 FROM Users
   LEFT OUTER JOIN
-    BeerReview
-  ON BeerReview.UserName = Users.UserName
+    BeerReviews
+  ON BeerReviews.UserName = Users.UserName
   LEFT OUTER JOIN
     ViewHistory
   ON ViewHistory.UserName = Users.UserName
-  WHERE BeerReview.ReviewId is NULL and ViewHistory.ViewId is NULL;
-  
-#10 On average, how many food pairings options does any one beer style have?
-SELECT AVG(T.Pairing_Counts) as Average_Pairing
+  WHERE BeerReviews.ReviewId IS NULL AND ViewHistory.ViewId IS NULL;
+
+# 10 On average, how many food pairings options does any one beer style have?
+SELECT ROUND(AVG(T.Pairing_Counts), 0) AS Average_Pairing
 FROM(
   SELECT BeerStyles.Style, COUNT(*) AS Pairing_Counts
   FROM BeerStyles LEFT OUTER JOIN Food
