90e870b256b7861023e2a2d40a3f00040b9f5b16
nathanjy212 <62783636+nathanjy212@users.noreply.github.com>
2023-03-21 15:10:48 -0700

    committing Users, Administrators and Persons

---
+++ deliverables/milestone-4/nathan/model/Administrators.java
@@ -0,0 +1,37 @@
+package BeerApp.model;
+
+
+/**
+ * Administrators is a simple, plain old java objects (POJO).
+ * Well, almost (it extends {@link Persons}).
+ */
+public class Administrators extends Persons {
+
+	public Administrators(String userName) {
+		super(userName);
+
+	}
+	
+	
+	
+//	protected Date lastLogin;
+//	
+//	public Administrators(String userName, String firstName, String lastName, Date lastLogin) {
+//		super(userName, firstName, lastName);
+//		this.lastLogin = lastLogin;
+//	}
+//	
+//	public Administrators(String userName) {
+//		super(userName);
+//	}
+//
+//	/** Getters and setters. */
+//	
+//	public Date getLastLogin() {
+//		return lastLogin;
+//	}
+//
+//	public void setLastLogin(Date lastLogin) {
+//		this.lastLogin = lastLogin;
+//	}	
+}
---
+++ deliverables/milestone-4/nathan/model/Users.java
@@ -0,0 +1,14 @@
+package BeerApp.model;
+
+
+/**
+ * BlogUsers is a simple, plain old java objects (POJO).
+ * Well, almost (it extends {@link Persons}).
+ */
+public class Users extends Persons {
+
+	public Users(String userName) {
+		super(userName);
+	}
+	
+}
