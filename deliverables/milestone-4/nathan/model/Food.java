aa9df691e755536e719aecd16b2dcb8129c387e0
Sifan Wei <sifanw1998@gmail.com>
2023-03-21 22:10:00 -0700

    Added BeerStyles, Food

---
+++ deliverables/milestone-4/nathan/model/Food.java
@@ -0,0 +1,26 @@
+package BeerApp.model;
+
+public class Food {
+    protected String foodName;
+    protected BeerStyles style;
+    public Food(String foodName, BeerStyles style) {
+        this.foodName = foodName;
+        this.style = style;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public BeerStyles getStyle() {
+        return style;
+    }
+
+    public void setStyle(BeerStyles style) {
+        this.style = style;
+    }
+}
