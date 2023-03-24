1a22b6b03fc2c75b1c0b009285d9be042e0f98ed
deathbysora <thephilipquan@gmail.com>
2023-03-23 20:33:45 -0700

    feature: app finished for pm4

--- BeerApp/src/main/java/beerapp/tools/Inserter.java
+++ BeerApp/src/main/java/beerapp/tools/Inserter.java
@@ -15,11 +15,16 @@ import beerapp.model.Food;
 import beerapp.model.Person;
 import beerapp.model.User;
 import java.sql.SQLException;
-import java.util.List;
 
 public class Inserter {
 
     public static void main(String[] args) throws SQLException {
+       mockApp();
+       mockBrewers();
+        mockFindSimilarBeers();
+    }
+
+    private static void mockApp() throws SQLException {
         // DAO instances.
         PersonsDao personsDao = PersonsDao.getInstance();
         AdministratorsDao administratorsDao = AdministratorsDao.getInstance();
@@ -29,96 +34,113 @@ public class Inserter {
         FoodDao foodDao = FoodDao.getInstance();
 
         // INSERT objects from our model.
-        Person person1 = new Person("b");
+        Person person1 = new Person("nathan");
         person1 = personsDao.create(person1);
 
         // READ.
-        Person p1 = personsDao.getPersonByUsername("b");
-        System.out.format("Reading person: %s\n", p1.getUsername());
+        Person p1 = personsDao.getPersonByUsername("nathan");
+        System.out.format("Created and Reading person: %s\n", p1.getUsername());
 
         // Update
-        Person p3 = personsDao.updateUsername(p1, "b2");
+        Person p3 = personsDao.updateUsername(p1, "nathanUpdated");
         System.out.format("Updating Person: name:%s \n",
           p3.getUsername());
 
         // Delete.
+        System.out.println("Successfully deleted Person nathanUpdated.");
         personsDao.delete(p3);
 
+        // Spacing
+        System.out.println();
+
         // INSERT objects from our model.
-        Administrator admin1 = new Administrator("a");
+        Administrator admin1 = new Administrator("elsaTa");
         admin1 = administratorsDao.create(admin1);
 
         // READ.
-        Administrator a1 = administratorsDao.getAdministratorFromUserName("a");
-        System.out.format("Reading administrator: %s \n",
+        Administrator a1 = administratorsDao.getAdministratorFromUserName("elsaTa");
+        System.out.format("Created and reading administrator: %s \n",
           a1.getUsername());
 
         // Update
-        Administrator a3 = administratorsDao.updateUserName(a1, "a2");
-        System.out.format("Updating administrators: name:%s \n",
+        Administrator a3 = administratorsDao.updateUserName(a1, "panXu1");
+        System.out.format("Updating administrators from name elsaTa to :%s \n",
           a3.getUsername());
 
         // Delete.
+        System.out.println("Successfully deleted the administrator panXu1");
         administratorsDao.delete(a3);
 
+        // Spacing
+        System.out.println();
+
         // INSERT objects from our model.
-        User user1 = new User("u");
+        User user1 = new User("nathanY56");
         user1 = usersDao.create(user1);
 
+        User user2 = new User("deathBySora");
+        user2 = usersDao.create(user2);
+
         // READ.
-        User u1 = usersDao.getUserFromUserName("u");
-        System.out.format("Reading user: u:%s \n",
+        User u1 = usersDao.getUserFromUserName("nathanY56");
+        System.out.format("Successfully created and found user1: u:%s using the .getUserFromUserName() method\n",
           u1.getUsername());
 
-        // Update.
-        User u2 = usersDao.updateUserName(u1, "u2");
-        System.out.format("Updating users: name:%s \n",
+        User u2 = usersDao.getUserFromUserName("deathBySora");
+        System.out.format("Successfully created and found user2: u:%s using the .getUserFromUserName() method \n",
           u2.getUsername());
 
+        // Update.
+        User u3 = usersDao.updateUserName(u1, "newNathanY56");
+        System.out.format("Updating users: from nathanY56 to new name:%s \n",
+          u3.getUsername());
+
         // Delete.
-        usersDao.delete(u2);
+        System.out.format("Successfully deleted deathbysora \n");
+        usersDao.delete(usersDao.getUserFromUserName("deathbysora"));
+        System.out.format("Successfully deleted " + u2.getUsername() + "\n");
+        usersDao.delete(u3);
+
+        // Spacing
+        System.out.println();
+
 
         // Insert BeerStyle & Food
-        BeerStyle beerStyle1 = new BeerStyle("BS1");
+        BeerStyle beerStyle1 = new BeerStyle("Belgian Ale");
         beerStyle1 = beerStyleDao.create(beerStyle1);
-        BeerStyle beerStyle2 = new BeerStyle("BS2");
+        BeerStyle beerStyle2 = new BeerStyle("Larger");
         beerStyle2 = beerStyleDao.create(beerStyle2);
 
-        Food food1 = new Food("f1", beerStyle1);
+        Food food1 = new Food("Pasta", beerStyle1);
         food1 = foodDao.create(food1);
-        Food food2 = new Food("f2", beerStyle2);
+        Food food2 = new Food("Pizza", beerStyle2);
         food2 = foodDao.create(food2);
 
         // Read BeerStyles & Food
-        BeerStyle bs1 = beerStyleDao.getBeerStyle("BS1");
-        System.out.format("Reading beerStyle: s:%s \n",
+        BeerStyle bs1 = beerStyleDao.getBeerStyle("Belgian Ale");
+        System.out.format("Successfully created and reading beerStyle: s:%s \n",
           bs1.getStyle());
 
-        List<Food> fList1 = foodDao.getFoodByStyle(beerStyle1);
-        for (Food f : fList1) {
-            System.out.format("Reading food: s:%s \n",
-              f.getStyle().getStyle());
-        }
-
-        // Update BeerStyle & food
-        BeerStyle bs2 = beerStyleDao.updateStyle(beerStyle1, "BS3");
-        System.out.format("Updating beerStyle: s:%s \n",
+        BeerStyle bs2 = beerStyleDao.getBeerStyle("Larger");
+        System.out.format("Successfully created and reading beerStyle: s:%s \n",
           bs2.getStyle());
 
-        Food f1 = foodDao.updateFoodName(food2, "f3");
-        System.out.format("Updating food: f:%s \n",
+        // Update BeerStyle & food
+        BeerStyle bs3 = beerStyleDao.updateStyle(beerStyle1, "IPA");
+        System.out.format("Updating beerStyle1 to new beerStyle3: s:%s \n",
+          bs3.getStyle());
+
+        Food f1 = foodDao.updateFoodName(food2, "Fries");
+        System.out.format("Updating food: from pizza to f:%s \n",
           f1.getFoodName());
 
+        beerStyleDao.delete(beerStyleDao.getBeerStyle("larger"));
+        beerStyleDao.delete(beerStyleDao.getBeerStyle("ipa"));
         // Delete BeerStyle & Food
-        foodDao.getFoodByName("f1").forEach((f) -> foodDao.delete(f));
-        foodDao.getFoodByName("f3").forEach((f) -> foodDao.delete(f));
-        beerStyleDao.delete(beerStyleDao.getBeerStyle("bs2"));
-        beerStyleDao.delete(beerStyleDao.getBeerStyle("bs3"));
-
-        mockBrewers();
-        mockFindSimilarBeers();
+        foodDao.getFoodByName("fries").forEach((f) -> foodDao.delete(f));
+        foodDao.getFoodByName("pasta").forEach((f) -> foodDao.delete(f));
+        foodDao.getFoodByName("pizza").forEach((f) -> foodDao.delete(f));
     }
-
     private static void mockBrewers() {
         BrewersDao brewersDao = BrewersDao.getInstance();
 
@@ -145,8 +167,9 @@ public class Inserter {
 
     private static void mockFindSimilarBeers() {
         BeersDao beersDao = BeersDao.getInstance();
-        Beer beer = new Beer(2, "Abeta Amber Lager", Float.valueOf(5), 1, new BeerStyle("Vienna"));
 
-        beersDao.getSimilarBeers(beer).forEach((b) -> System.out.println(b.getBeer()));
+        Beer beer = new Beer(2, "Abeta Amber Lager", Float.valueOf(5), 1, new BeerStyle("Vienna"));
+        System.out.println("Find similarly rated beers for: " + beer.getName());
+        beersDao.getSimilarBeers(beer).forEach((b) -> System.out.println("* " + b.getName()));
     }
 }
