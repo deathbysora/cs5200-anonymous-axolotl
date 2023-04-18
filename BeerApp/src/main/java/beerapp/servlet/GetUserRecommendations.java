398107b666853b136baafff2f9a195b4c0a1e5fd
danielyhbi <danielyhbi@gmail.com>
2023-04-17 23:12:35 -0700

    update personal recommendations

---
+++ BeerApp/src/main/java/beerapp/servlet/GetUserRecommendations.java
@@ -0,0 +1,56 @@
+package beerapp.servlet;
+
+import beerapp.dal.BeerReviewsDao;
+import beerapp.dal.BeersDao;
+import beerapp.model.Beer;
+import beerapp.model.BeerReview;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+@WebServlet("/getuserrecommendations")
+public class GetUserRecommendations extends HttpServlet {
+
+    protected BeerReviewsDao beerReviewsDao;
+
+    @Override
+    public void init() throws ServletException {
+        beerReviewsDao = BeerReviewsDao.getInstance();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // Map for storing messages.
+        // Map<String, String> messages = new HashMap<>();
+        // req.setAttribute("messages", messages);
+        List<List<String>> top10Recs = new ArrayList<>();
+
+        try {
+            int aroma = Integer.parseInt(req.getParameter("aroma"));
+            int palate = Integer.parseInt(req.getParameter("palate"));
+            int taste = Integer.parseInt(req.getParameter("taste"));
+            int appearance = Integer.parseInt(req.getParameter("appearance"));
+
+            top10Recs = beerReviewsDao.getPersonalizedBeerRecommendations(palate, taste, aroma, appearance);
+
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = new Gson().toJson(top10Recs);
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(json);
+
+    }
+}
